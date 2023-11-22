d5f46cd9aebc3a9ee45318514e24e50d41d0788b
Tanvir_Hossen <tanvirhossen853@gmail.com>
2023-11-22 15:15:39 +0600

    TypeScript first program and Environment setup

---
+++ index.js
@@ -0,0 +1,13 @@
+var userName = "Tanvir Hossen raju";
+var age = 26;
+console.log(userName);
+console.log(age);
+// tsc index.ts--> index.js-->node index.js
+// tsc index.ts --watch--> index.js-->node index.js
+
+
+function addNumbers(num1:number,num2:number){
+    console.log(num1+num2)
+}
+addNumbers(20,30)
+addNumbers(20,"30")
---
+++ index.ts
@@ -0,0 +1,8 @@
+let userName = "Tanvir Hossen raju";
+let age=26
+console.log(userName);
+console.log(age)
+
+
+// tsc index.ts--> index.js-->node index.js
+// tsc index.ts --watch--> index.js-->node index.js
