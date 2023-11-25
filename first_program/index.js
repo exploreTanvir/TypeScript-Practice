663cb3db2f5247726e511b20127748e760ea4c5f
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2023-11-25 22:58:51 +0600

    First program using TypeScript

---
+++ first_program/index.js
@@ -0,0 +1,6 @@
+var userName = "Tanvir Hossen raju";
+var age = 26;
+console.log(userName);
+console.log(age);
+// tsc index.ts--> index.js-->node index.js
+// tsc index.ts --watch--> index.js-->node index.js
