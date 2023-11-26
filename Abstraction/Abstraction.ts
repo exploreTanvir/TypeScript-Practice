63127a25835c287d57f34889b1da6386113dd537
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2023-11-26 23:20:44 +0600

    Abstraction

---
+++ Abstraction/Abstraction.ts
@@ -0,0 +1,36 @@
+abstract class User {
+  userName: string;
+  age: number;
+
+  constructor(userName: string, age: number) {
+    this.userName = userName;
+    this.age = age;
+  }
+
+  abstract display(): void
+}
+// Object
+//   let user1 = new User("Tanvir", 123);
+//   user1.display()
+
+//   let user2 = new User("Raju", 456);
+//   user2.display()
+
+//Inherite class
+
+class Student extends User {
+  StudentId: number;
+
+  constructor(userName: string, age: number, StudentId: number) {
+    super(userName, age);
+    this.StudentId = StudentId;
+  }
+  display(): void {
+    console.log(
+      `UserName: ${this.userName}, age: ${this.age}, ID: ${this.StudentId}`
+    );
+  }
+}
+
+let Student1 = new Student("Tanvir", 123, 532987);
+Student1.display();
