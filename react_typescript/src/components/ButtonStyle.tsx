2b9692b8b9ebd0978e18c4a93e75d4fe78a0416a
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2023-11-30 21:54:27 +0600

    typing style props

--- react_typescript/src/components/ButtonStyle.tsx
+++ react_typescript/src/components/ButtonStyle.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-
-const ButtonStyle = () => {
+import React from "react";
+type btnProps = {
+  btnStyle: React.CSSProperties;
+};
+const ButtonStyle = (props:btnProps) => {
   return (
     <div>
-        <h1>Style props from ButtonStyle.tsx file</h1>
-        <h5 style={{color:"red"}}>ButtonStyle</h5>
+      <h1>Style props from ButtonStyle.tsx file</h1>
+      <h5 style={props.btnStyle}>ButtonStyle</h5>
     </div>
-  )
-}
+  );
+};
 
-export default ButtonStyle
+export default ButtonStyle;
